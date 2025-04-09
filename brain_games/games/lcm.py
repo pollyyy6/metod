fa85357bafbcaf7a764edb71b0336a4a45f2a507
pollyyy6 <62114421+pollyyy6@users.noreply.github.com>
2025-04-09 12:25:45 +0300

    updated all 2.0

--- brain_games/games/lcm.py
+++ brain_games/games/lcm.py
@@ -6,17 +6,14 @@ DESCRIPTION = "Find the smallest common multiple of given numbers."
 
 
 def get_random_numbers():
-    """Генерирует 3 случайных числа от 1 до 100"""
     return [random.randint(1, 20) for _ in range(3)]
 
 
 def lcm(a, b):
-    """Вычисляет НОК двух чисел"""
     return a * b // math.gcd(a, b)
 
 
 def lcm_of_three(a, b, c):
-    """Вычисляет НОК трёх чисел"""
     return lcm(lcm(a, b), c)
 
 
